fa45d35667f41f7a5ea5df9e458886f7d4415536
Walter Eaves <walter.eaves@datasciencesconsulting.com>
2015-01-09 11:20:13 +0000

    -

---
+++ script/DataSciences0/Get-IPDetails.psm1
@@ -0,0 +1,72 @@
+function global:Get-IPDetails {
+<#
+
+            .SYNOPSIS
+
+            Lists IP details
+
+            .DESCRIPTION
+
+            Lists the details of all the network interfaces: network,
+
+
+            .EXAMPLE
+
+            ADD-LOGFILE
+
+ Creates a Logfile in C:\Logfile folder
+
+
+            .NOTES
+
+            Creates logfiles based upon Date and Time
+ 
+
+            .LINK
+
+            http://www.powershell.ca
+
+#>
+
+[cmdletbinding(
+DefaultParameterSetName="Folder",
+SupportsShouldProcess=$True,
+ConfirmImpact='High'
+)]
+
+param (
+ [parameter(ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+    [string[]]$ComputerName = $env:computername
+)            
+
+begin {}
+process {
+ foreach ($Computer in $ComputerName) {
+  if(Test-Connection -ComputerName $Computer -Count 1 -ea 0) {
+   $Networks = Get-WmiObject Win32_NetworkAdapterConfiguration -ComputerName $Computer | ? {$_.IPEnabled}
+   foreach ($Network in $Networks) {
+    $IPAddress  = $Network.IpAddress[0]
+    $SubnetMask  = $Network.IPSubnet[0]
+    $DefaultGateway = $Network.DefaultIPGateway
+    $DNSServers  = $Network.DNSServerSearchOrder
+    $IsDHCPEnabled = $false
+    If($network.DHCPEnabled) {
+     $IsDHCPEnabled = $true
+    }
+    $MACAddress  = $Network.MACAddress
+    $OutputObj  = New-Object -Type PSObject
+    $OutputObj | Add-Member -MemberType NoteProperty -Name ComputerName -Value $Computer.ToUpper()
+    $OutputObj | Add-Member -MemberType NoteProperty -Name IPAddress -Value $IPAddress
+    $OutputObj | Add-Member -MemberType NoteProperty -Name SubnetMask -Value $SubnetMask
+    $OutputObj | Add-Member -MemberType NoteProperty -Name Gateway -Value $DefaultGateway
+    $OutputObj | Add-Member -MemberType NoteProperty -Name IsDHCPEnabled -Value $IsDHCPEnabled
+    $OutputObj | Add-Member -MemberType NoteProperty -Name DNSServers -Value $DNSServers
+    $OutputObj | Add-Member -MemberType NoteProperty -Name MACAddress -Value $MACAddress
+    $OutputObj
+   }
+  }
+ }
+}            
+
+end {}
+}
--- script/creds.ps1
+++ script/creds.ps1
@@ -14,13 +14,70 @@ $mycreds = New-Object System.Management.Automation.PSCredential ("owner", $secpa
 $abdul = New-PSSession -Authentication "Negotiate" -ComputerName abdul -Credential $mycreds
 
 $ScriptBlockContent = {
-    param ($MessageToWrite, $mesg)
-    Write-Host $MessageToWrite 
-    Write-Host $mesg
+    param ($ip, $name)
+    Add-Content -Encoding ASCII -Path "c:\Windows\System32\drivers\etc\hosts" -Value "$ip $name"
     }
 
-Invoke-Command -Session $abdul -ScriptBlock $ScriptBlockContent -ArgumentList "DNS1", "DNS2"
+Invoke-Command -Session $abdul -ScriptBlock $ScriptBlockContent -ArgumentList "172.24.29.17", "l3.site"
 
-$job = Test-connection -Count 1 -ComputerName (Get-Content ..\etc\slaves) -asJob
+<#
 
-if ($job.JobStateInfo.State -ne "Running") { $results = Receive-Job $job }
+ping test: what hosts are up?
+
+The orega.com network returns a ping for every host! 
+
+#>
+
+function FileSize3 ($dir, $minSize)
+{
+  dir $dir
+  where {$_.length -gt $minsize}
+}
+
+$slaves = @()
+
+$slaves0 = Test-connection -Count 1 -ComputerName (Get-Content ..\etc\slaves)
+
+foreach ($h0 in ($slaves0)) {
+    if ($h0.IPV4Address.IPAddressToString -like '172.24.29.*') {
+        $slaves += $h0.Address
+    }
+}
+
+Write-Host $slaves
+
+function Pinger($ComputerName) 
+{
+    $slaves = @()
+
+    $slaves0 = Test-connection -Count 1 -ComputerName $ComputerName
+
+    foreach ($h0 in ($slaves0)) {
+	if ($h0.IPV4Address.IPAddressToString -like '172.24.29.*') {
+            $slaves += $h0.Address
+	}
+    }
+
+}
+
+
+
+function sobject {
+    param(
+          [Parameter(Mandatory=$true)]
+          [String]$name
+    )
+    
+    $s0 = $sobject.psobject.copy()
+    $s0.name = $name
+    $s0.network = "172.24.29.0"
+    $s0
+}
+
+function Network0
+
+foreach ($iface in (Get-NetConnectionProfile)) {
+    if ($iface.InterfaceAlias -in "Ethernet", "Wi-Fi") {
+        Write-Host $iface.InterfaceAlias
+        Set-NetConnectionProfile -InterfaceAlias $iface.InterfaceAlias -NetworkCategory Private
+    }
